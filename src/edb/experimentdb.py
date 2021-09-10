1cba946897fe3636d51ea1e5be5da8193855ed25
Scott Wales <scott.wales@unimelb.edu.au>
2021-09-10 17:02:04 +1000

    Add null check

--- src/edb/experimentdb.py
+++ src/edb/experimentdb.py
@@ -219,16 +219,16 @@ class ExperimentDB:
         sel = search_filter(sel, **kwargs)
 
         if time is not None:
-            t0 = xarray.coding.cftime_offsets.to_cftime_datetime(
-                time.start, calendar="366_day"
-            )
-            if t0 is not None:
+            if time.start is not None:
+                t0 = xarray.coding.cftime_offsets.to_cftime_datetime(
+                    time.start, calendar="366_day"
+                )
                 sel = sel.where(db.file.c.end_date >= str(t0))
 
-            t1 = xarray.coding.cftime_offsets.to_cftime_datetime(
-                time.stop, calendar="366_day"
-            )
-            if t1 is not None:
+            if time.stop is not None:
+                t1 = xarray.coding.cftime_offsets.to_cftime_datetime(
+                    time.stop, calendar="366_day"
+                )
                 sel = sel.where(db.file.c.start_date <= str(t1))
 
         df = pandas.read_sql(
