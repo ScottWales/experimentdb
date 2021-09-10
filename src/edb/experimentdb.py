e952d4b145353c11caaf3efd7fd5e56317065711
Scott Wales <scott.wales@unimelb.edu.au>
2021-09-10 16:57:13 +1000

    Blacken

--- src/edb/experimentdb.py
+++ src/edb/experimentdb.py
@@ -219,11 +219,15 @@ class ExperimentDB:
         sel = search_filter(sel, **kwargs)
 
         if time is not None:
-            t0 = xarray.coding.cftime_offsets.to_cftime_datetime(time.start, calendar="366_day")
+            t0 = xarray.coding.cftime_offsets.to_cftime_datetime(
+                time.start, calendar="366_day"
+            )
             if t0 is not None:
                 sel = sel.where(db.file.c.end_date >= str(t0))
 
-            t1 = xarray.coding.cftime_offsets.to_cftime_datetime(time.stop, calendar="366_day")
+            t1 = xarray.coding.cftime_offsets.to_cftime_datetime(
+                time.stop, calendar="366_day"
+            )
             if t1 is not None:
                 sel = sel.where(db.file.c.start_date <= str(t1))
 
